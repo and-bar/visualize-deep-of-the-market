105a6c57f0444470de5805aba35d0be1111b0bdb
Andriy Baran <andriybaran@gmail.com>
2024-01-14 21:35:35 -0500

    small correction, deletion from tick data small volumes for quicker drawing on canvas. Next base stage version

--- visualize_deep_of_the_market_tkinter_draw_without_graphic_library.py
+++ visualize_deep_of_the_market_tkinter_draw_without_graphic_library.py
@@ -26,21 +26,26 @@ def load_pickle_files(directory):
     return concatenated_list
 
 directory = 'C:/Temp/dom request data'
-scope_data = sorted(load_pickle_files(directory), key=lambda x: x[0]) # sort of tickes based on index
+full_data_of_ticks = sorted(load_pickle_files(directory), key=lambda x: x[0]) # sort of tickes based on index
 
-scope_data[0]
+# with purpose of quicker visualization from tick data will be deleted small volumes for beter visual representation
+boundry_of_volume_for_deletion = 500000
+scope_data = [[sublist[0], [pair for pair in sublist[1] if pair[1] >= boundry_of_volume_for_deletion], [pair for pair in sublist[2] if pair[1] >= boundry_of_volume_for_deletion], sublist[3]] for sublist in full_data_of_ticks]
+
+# scope_data = full_data_of_ticks
 
 def draw_dom_data_on_canvas (canvas, dom_data_full, end_tick_bar_to_draw):
     """draw dom data on the canvas"""
     # logic of how many ticks can be visualized on width of canvas
     if canvas.winfo_width() == 1:
-        maximum_with_canvas = 3000
-        max_pixel_vertical = 2000
+        maximum_with_canvas = 1027
+        max_pixel_vertical = 980
     else:
         maximum_with_canvas = canvas.winfo_width()
-        max_pixel_vertical = canvas.winfo_height()
+        max_pixel_vertical = canvas.winfo_height() - 20 # -20 here for allowing drawing on the canvas minimum bid presented
     # getting info how many tick can fir into canvas with
     space_between_volume_bars = 2
+    height_of_volume_bar_in_pixels = 10
     canvas_with_left = maximum_with_canvas #3840
     dom_data = dom_data_full[:end_tick_bar_to_draw]
     lenght_ofdom_data_elements = len(dom_data)
@@ -49,7 +54,7 @@ def draw_dom_data_on_canvas (canvas, dom_data_full, end_tick_bar_to_draw):
         bid_prices, bid_volumes = zip(*dom_data[n_tick][1])
         ask_prices, ask_volumes = zip(*dom_data[n_tick][2])
         # scaling here volume n000000 to one pixel
-        one_pixel_equeal_n_volume = 5000
+        one_pixel_equeal_n_volume = 65000
         bid_volumes_pixels = [round(element / one_pixel_equeal_n_volume) for element in bid_volumes]
         ask_volumes_pixels = [round(element / one_pixel_equeal_n_volume) for element in ask_volumes]
         max_volume_pixel = max(max(bid_volumes_pixels), max(ask_volumes_pixels))
@@ -58,7 +63,7 @@ def draw_dom_data_on_canvas (canvas, dom_data_full, end_tick_bar_to_draw):
         if canvas_with_left - max_volume_pixel <= 0:
             break
         n_tick -= 1
-        print("max_volume_pixel: ", max_volume_pixel, " min volume pixel: ", min(min(bid_volumes_pixels), min(ask_volumes_pixels)), "min volume in usd: ", one_pixel_equeal_n_volume*min(min(bid_volumes_pixels), min(ask_volumes_pixels)))
+        # print("max_volume_pixel: ", max_volume_pixel, " min volume pixel: ", min(min(bid_volumes_pixels), min(ask_volumes_pixels)), "min volume in usd: ", one_pixel_equeal_n_volume*min(min(bid_volumes_pixels), min(ask_volumes_pixels)))
 
     n_of_ticks_to_visualize = lenght_ofdom_data_elements - n_tick+1
     start_index_tick_data = n_tick+1
@@ -80,16 +85,19 @@ def draw_dom_data_on_canvas (canvas, dom_data_full, end_tick_bar_to_draw):
         scaling_factor = (max_pixel_vertical - min_pixel_vertical) / (max_price - min_price)
         # Map each price to its pixel value
         bid_pixels = [int(max_pixel_vertical - (price - min_price) * scaling_factor) for price in bid_prices]
+        # print("bid pixels: ", bid_pixels, "max bid pixel: ", max(bid_pixels))
+        # print("bid volume pixels: ", bid_volumes_pixels)
         ask_pixels = [int(max_pixel_vertical - (price - min_price) * scaling_factor) for price in ask_prices]
         max_volume_pixel = max(max(bid_volumes_pixels), max(ask_volumes_pixels))
         bid_volume_pixels = list(zip(bid_pixels, bid_volumes_pixels))
         ask_volume_pixels = list(zip(ask_pixels, ask_volumes_pixels))
         top_left_coordinate_left = canvas_with_left - max_volume_pixel - space_between_volume_bars
         # drawing volumes on the
-        [canvas.create_rectangle(top_left_coordinate_left, price_bid_pixel, top_left_coordinate_left + volume_bid_pixel, price_bid_pixel+10, fill="green") for price_bid_pixel, volume_bid_pixel in bid_volume_pixels] #bids
-        [canvas.create_rectangle(top_left_coordinate_left, price_ask_pixel, top_left_coordinate_left + volume_ask_pixel, price_ask_pixel+10, fill="red") for price_ask_pixel, volume_ask_pixel in ask_volume_pixels] #asks
+        [canvas.create_rectangle(top_left_coordinate_left, price_bid_pixel, top_left_coordinate_left + volume_bid_pixel, price_bid_pixel+height_of_volume_bar_in_pixels, fill="green") for price_bid_pixel, volume_bid_pixel in bid_volume_pixels] #bids
+        [canvas.create_rectangle(top_left_coordinate_left, price_ask_pixel, top_left_coordinate_left + volume_ask_pixel, price_ask_pixel+height_of_volume_bar_in_pixels, fill="red") for price_ask_pixel, volume_ask_pixel in ask_volume_pixels] #asks
+        # print("min ask volume in pixels: ", max([ price_bid_pixel for price_bid_pixel, volume_bid_pixel in bid_volume_pixels]))
         canvas_with_left = top_left_coordinate_left
-    root.after(1000, lambda: draw_dom_data_on_canvas(canvas, dom_data_full, end_tick_bar_to_draw + 1))
+    root.after(10, lambda: draw_dom_data_on_canvas(canvas, dom_data_full, end_tick_bar_to_draw + 1))
 
 root = tk.Tk()
 root.title("EUR USD deep of the market")
